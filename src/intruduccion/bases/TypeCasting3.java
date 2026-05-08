b83550393b072330c0991949605b77e104d9ce33
Carlos Eduardo Corado <ccoradoi1@miumg.edu.gt>
2026-05-07 21:53:26 -0600

    ejemplos 3 y 4 TypeCasting

---
+++ src/intruduccion/bases/TypeCasting3.java
@@ -0,0 +1,21 @@
+package intruduccion.bases;
+
+public class TypeCasting3 {
+
+		  public static void main(String[] args) {
+
+		    // create int type variable
+
+		    int num = 50;
+
+		    System.out.println("The integer value is: " + num);
+
+		    // convert int to string type
+
+		    String data = String.valueOf(num);
+
+		    System.out.println("The string value is: " + data);
+
+		  }
+
+}
---
+++ src/intruduccion/bases/TypeCasting4.java
@@ -0,0 +1,21 @@
+package intruduccion.bases;
+
+public class TypeCasting4 {
+
+		  public static void main(String[] args) {
+
+		    // create string type variable
+
+		    String data = "50";
+
+		    System.out.println("The string value is: " + data);
+
+		    // convert string variable to int
+
+		    int num = Integer.parseInt(data);
+
+		    System.out.println("The integer value is: " + num);
+
+		  }
+		  
+}
