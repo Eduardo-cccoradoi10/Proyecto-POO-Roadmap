5040f13fd4a4cfed903c24e99dc4d5d27556182a
Carlos Eduardo Corado <ccoradoi1@miumg.edu.gt>
2026-05-07 21:50:23 -0600

    ejemplos 1 y 2 type casting

--- src/intruduccion/bases/TypeCasting.java
+++ src/intruduccion/bases/TypeCasting.java
@@ -2,4 +2,21 @@ package intruduccion.bases;
 
 public class TypeCasting {
 
+		  public static void main(String[] args) {
+
+		    // create int type variable
+
+		    int num = 50;
+
+		    System.out.println("The integer value: " + num);
+
+		    // convert into double type
+
+		    double data = num;
+
+		    System.out.println("The double value: " + data);
+
+		  }
+
+		
 }
--- src/intruduccion/bases/TypeCasting2.java
+++ src/intruduccion/bases/TypeCasting2.java
@@ -2,4 +2,20 @@ package intruduccion.bases;
 
 public class TypeCasting2 {
 
+		  public static void main(String[] args) {
+
+		    // create double type variable
+
+		    double num = 50.55;
+
+		    System.out.println("The double value: " + num);
+
+		    // convert into int type
+
+		    int data = (int)num;
+
+		    System.out.println("The integer value: " + data);
+
+		  }
+
 }
