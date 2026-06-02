2ba5359b3a145d510e2e949b659a6182dc4a44bd
Carlos Eduardo Corado <ccoradoi1@miumg.edu.gt>
2026-06-02 12:10:46 -0600

    añadiendo ejemplos de enums vistos

---
+++ src/intruduccion/poo/enums/Ejemplo1.java
@@ -0,0 +1,14 @@
+package intruduccion.poo.enums;
+
+public class Ejemplo1 {
+	enum Size {
+		   SMALL, MEDIUM, LARGE, EXTRALARGE
+		}
+
+	
+		   public static void main(String[] args) {
+		      System.out.println(Size.SMALL);
+		      System.out.println(Size.MEDIUM);
+		   }
+
+}
