8768794a2dd09d82c6412d16c59b6fa77c9cd293
Carlos Eduardo Corado <ccoradoi1@miumg.edu.gt>
2026-06-05 23:10:12 -0600

    ejemplo de sobreescritura finalizado

--- src/intruduccion/sobreescritura/Animal.java
+++ src/intruduccion/sobreescritura/Animal.java
@@ -1,5 +1,16 @@
 package intruduccion.sobreescritura;
 
-public class Animal {
+class Animal {
 
+    void hacerSonido() {
+        System.out.println("El animal hace un sonido");
+    }
+}
+
+class Perro extends Animal {
+
+    @Override
+    void hacerSonido() {
+        System.out.println("El perro ladra");
+    }
 }
